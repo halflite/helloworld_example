44ddcc15e07044f595cdc3d87fd58baaddebe38c
halflite <shingo@halflite.net>
2024-07-13 06:40:29 +0900

    各ライブラリ追加

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # helloworld_example
 Java Hello World
+
+## compile & package
+
+```bash
+cd web
+mvn clean package
+java -jar target/helloworld-1.0-SNAPSHOT.jar
+```
---
+++ web/src/main/java/web/App.java
@@ -0,0 +1,14 @@
+package web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Main App */
+public class App {
+  /** logger */
+  private final static Logger LOG = LoggerFactory.getLogger(App.class);
+
+  public static void main(String[] args) {
+    LOG.info("started.");
+  }
+}
